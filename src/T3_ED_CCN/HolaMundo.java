383df38b90e333564ca0d39ef9d5bfe292f33526
Carlos <carloscamachonunez@gmail.com>
2022-11-29 20:03:07 +0100

    adios

--- src/T3_ED_CCN/HolaMundo.java
+++ src/T3_ED_CCN/HolaMundo.java
@@ -1,5 +1,10 @@
 package T3_ED_CCN;
 
 public class HolaMundo {
-
+public static void main(String[] args) {
+	
+	
+	System.out.println("Hola mundo soy Carlos Camacho");
+	System.out.println("adios");
+}
 }
